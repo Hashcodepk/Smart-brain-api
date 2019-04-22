00cd1ea631bb7a6de6437d9b94d7578d2a9f6f6b
Hashcodepk <hashirlives@gmail.com>
2019-04-22 08:31:21 -0700

    uploading files

--- controllers/image.js
+++ controllers/image.js
@@ -2,7 +2,7 @@ const Clarifai = require('clarifai');
 
 //You must add your own API key here from Clarifai.
 const app = new Clarifai.App({
- apiKey: 'YOUR_API_KEY_HERE'
+ apiKey: 'a1c9b6c53f0c4a37bc7930ca5e3ca293'
 });
 
 const handleApiCall = (req, res) => {
